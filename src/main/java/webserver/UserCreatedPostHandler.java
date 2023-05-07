6f72c7bfe4132e51ab02a8084128ccc097d328ee
youngduklee <bd_lee@bigin.io>
2023-05-07 11:56:11 +0900

    etc: 주석 제거

--- src/main/java/webserver/UserCreatedPostHandler.java
+++ src/main/java/webserver/UserCreatedPostHandler.java
@@ -1,12 +1,3 @@
-/*
- * Created by Be lee on 2023/05/06
- * As part of Bigin
- *
- * Copyright (C) Bigin (https://bigin.io/main) - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by backend Team <bd_lee@bigin.io>, 2023/05/06
- */
 package webserver;
 
 import db.DataBase;
@@ -19,16 +10,6 @@ import model.User;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
-/**
- * create on 2023/05/06. create by IntelliJ IDEA.
- *
- * <p> 클래스 설명 </p>
- * <p> {@link } and {@link }관련 클래스 </p>
- *
- * @author Bd Lee
- * @version 1.0
- * @since 1.0
- */
 public class UserCreatedPostHandler extends FrontHandler {
 
   private final BufferedReader request;
